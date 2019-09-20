ddc01563d7d2f0bad6c4f06b180eb84b1414d954
IS97575 <IS97575@T1LPRVT3414.gm.isbank.com.tr>
2019-09-20 15:07:27 +0300

    Initial commit after maven.

---
+++ basic-work/src/main/java/com/omerkeskin/unittesting/basicwork/BasicWorkApplication.java
@@ -0,0 +1,13 @@
+package com.omerkeskin.unittesting.basicwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicWorkApplication.class, args); 
+	}
+
+}
