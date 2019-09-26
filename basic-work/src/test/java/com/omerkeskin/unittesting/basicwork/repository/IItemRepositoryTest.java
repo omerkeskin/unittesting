0bf446da0d55bfec4c6238cd548e48cd81c51f91
IS97575 <IS97575@T1LPRVT3414.gm.isbank.com.tr>
2019-09-26 11:28:26 +0300

    Project structure fix

--- basic-work/src/test/java/com/omerkeskin/unittesting/basicwork/repository/IItemRepositoryTest.java
+++ basic-work/src/test/java/com/omerkeskin/unittesting/basicwork/repository/IItemRepositoryTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.omerkeskin.unittesting.basicwork.model.Item;
 
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class IItemRepositoryTest {
