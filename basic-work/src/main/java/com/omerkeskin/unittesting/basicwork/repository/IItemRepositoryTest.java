77bfab2d90b55e8075a83cce468cc51b42c86931
IS97575 <IS97575@T1LPRVT3414.gm.isbank.com.tr>
2019-09-26 10:27:01 +0300

    Repository and integration test.

---
+++ basic-work/src/main/java/com/omerkeskin/unittesting/basicwork/repository/IItemRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.omerkeskin.unittesting.basicwork.repository;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.omerkeskin.unittesting.basicwork.model.Item;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class IItemRepositoryTest {
+	
+	@Autowired
+	private IItemRepository itemRepository;
+
+	@Test
+	public void testFindAll() {
+		List<Item> lists = itemRepository.findAll();
+		assertEquals(3, lists.size());
+	}
+
+}
